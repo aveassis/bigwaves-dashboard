1168b7751708bc2628c10189d1155fd954de8377
Tristan Marcillaard <lindamarcillaud@gmail.com>
2026-05-05 19:58:11 +0000

    Knoppen nog breder: 2.0, 2.0, 2.2 ratio

--- dashboard.py
+++ dashboard.py
@@ -256,7 +256,7 @@ with hcol2:
         }
         </style>
         """, unsafe_allow_html=True)
-        _,b1,b2,b3=st.columns([0.05,1.5,1.5,1.7])
+        _,b1,b2,b3=st.columns([0.05,2.0,2.0,2.2])
         with b1:
             if st.button("📄 PDF", type="secondary", use_container_width=True):
                 try:
