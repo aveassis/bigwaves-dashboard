4946267842eb24afab940a5b71a14fa10201c5d3
Tristan Marcillaard <lindamarcillaud@gmail.com>
2026-05-16 15:26:25 +0000

    fix: missing closing quotes op toggle JS st.markdown

--- dashboard.py
+++ dashboard.py
@@ -76,7 +76,7 @@ st.markdown("""<script>
     checkSidebar();
     setInterval(checkSidebar, 500);
 })();
-</script>
+</script>""", unsafe_allow_html=True)
 
 st.markdown("""<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">""", unsafe_allow_html=True)
 
